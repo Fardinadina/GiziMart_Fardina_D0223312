99f664e36b26cccc69a47ea0b6b2942492462d3d
Fardinadina <fardinadina936@gmail.com>
2025-05-19 14:53:01 -1200

    hahi

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
 require __DIR__.'/auth.php';
 Route::middleware('auth', 'admin')->group(function(){
